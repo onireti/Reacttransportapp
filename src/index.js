5bc3b79eb84c93e235982b6b3dc9c44f1f7181f2
onireti <57454004+onireti@users.noreply.github.com>
2019-11-07 10:32:32 +0100

    main

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 import "./styles.css";
 import Navbar from "./components/Navbar";
+import footer from "./components/footer";
 
 function App() {
   return (
     <div>
       <Navbar />;
+      <footer />;
     </div>
   );
 }
